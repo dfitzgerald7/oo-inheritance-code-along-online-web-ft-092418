3c1ca1c67c722e0d65cd1d7a46879a0a9e8c7c6a
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 19:15:10 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,5 +3,8 @@ class Vehicle
   attr_accessor :wheel_size, :wheel_number 
   
   def initialize(wheel_size, wheel_number)
+    self.wheel_size = wheel_size
+    self.wheel_number = wheel_number
+  end 
 
 end
