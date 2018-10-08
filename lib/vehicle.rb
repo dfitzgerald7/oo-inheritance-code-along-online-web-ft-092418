605423c74c79749239916c116d075ceb4709b97f
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 19:15:24 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,5 +6,13 @@ class Vehicle
     self.wheel_size = wheel_size
     self.wheel_number = wheel_number
   end 
+  
+    def go
+    "vrrrrrrrooom!"
+  end
+ 
+  def fill_up_tank
+    "filling up!"
+  end
 
 end
